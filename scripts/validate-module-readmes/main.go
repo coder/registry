ce1a2353001e91ac794fbfe0c194bf78770ef9c1
Michael Smith <throwawayclover@gmail.com>
2025-04-09 19:41:59 +0000

    wip: commit progress

---
+++ scripts/validate-module-readmes/main.go
@@ -0,0 +1,91 @@
+// This package handles validating the READMEs of each module within the main
+// Registry directory
+package main
+
+import (
+	"log"
+	"os"
+	"path"
+
+	"coder.com/coder-registry/cmd/readme"
+)
+
+/*
+Things I need to do:
+1. Grab each README file
+2.
+*/
+
+type moduleFrontmatter struct {
+	Tags        []string `yaml:"tags"`
+	Description string   `yaml:"description"`
+	IconURL     string   `yaml:"icon"`
+	DisplayName *string  `yaml:"display_name"`
+}
+
+type moduleFrontmatterWithFilePath struct {
+	moduleFrontmatter
+	FilePath string
+}
+
+func aggregateModuleReadmeFiles() ([]readme.Readme, error) {
+	registryEntries, err := os.ReadDir(readme.RootRegistryPath)
+	if err != nil {
+		return nil, err
+	}
+
+	allReadmeFiles := []readme.Readme{}
+	problems := []error{}
+	for _, e := range registryEntries {
+		if !e.IsDir() {
+			continue
+		}
+
+		modulesPath := path.Join(readme.RootRegistryPath, e.Name(), "modules")
+		modEntries, err := os.ReadDir(modulesPath)
+		if err != nil {
+			problems = append(problems, err)
+			continue
+		}
+
+		for _, e := range modEntries {
+			if !e.IsDir() {
+				continue
+			}
+
+			readmePath := path.Join(modulesPath, "README.md")
+			rmBytes, err := os.ReadFile(readmePath)
+			if err != nil {
+				problems = append(problems, err)
+				continue
+			}
+			allReadmeFiles = append(allReadmeFiles, readme.Readme{
+				FilePath: readmePath,
+				RawText:  string(rmBytes),
+			})
+		}
+	}
+
+	if len(problems) != 0 {
+		return nil, readme.ValidationPhaseError{
+			Phase:  readme.ValidationPhaseFilesystemRead,
+			Errors: problems,
+		}
+	}
+
+	return allReadmeFiles, nil
+}
+
+func main() {
+	log.Println("Starting README validation for modules")
+	allReadmeFiles, err := aggregateModuleReadmeFiles()
+	if err != nil {
+		log.Panic(err)
+	}
+
+	if len(allReadmeFiles) == 0 {
+		log.Printf("No module files to process")
+		return
+	}
+	log.Printf("Processing %d README files\n", len(allReadmeFiles))
+}
