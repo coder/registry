8d8374937459ee7058c2acf532bbb5d3e69bfade
Charlie Voiselle <464492+angrycub@users.noreply.github.com>
2025-05-19 21:07:16 -0400

    add subdomain plumbing

--- registry/coder/modules/kasmvnc/main.tf
+++ registry/coder/modules/kasmvnc/main.tf
@@ -29,22 +29,29 @@ variable "kasm_version" {
 variable "desktop_environment" {
   type        = string
   description = "Specifies the desktop environment of the workspace. This should be pre-installed on the workspace."
+
   validation {
     condition     = contains(["xfce", "kde", "gnome", "lxde", "lxqt"], var.desktop_environment)
     error_message = "Invalid desktop environment. Please specify a valid desktop environment."
   }
 }
 
+variable "subdomain" {
+  type        = bool
+  default     = true
+  description = "Is subdomain sharing enabled in your cluster?"
+}
+
 resource "coder_script" "kasm_vnc" {
   agent_id     = var.agent_id
   display_name = "KasmVNC"
   icon         = "/icon/kasmvnc.svg"
-  script = templatefile("${path.module}/run.sh", {
-    PORT : var.port,
-    DESKTOP_ENVIRONMENT : var.desktop_environment,
-    KASM_VERSION : var.kasm_version
-  })
   run_on_start = true
+  script       = templatefile("${path.module}/run.sh", {
+    PORT                = var.port,
+    DESKTOP_ENVIRONMENT = var.desktop_environment,
+    KASM_VERSION        = var.kasm_version
+  })
 }
 
 resource "coder_app" "kasm_vnc" {
@@ -53,8 +60,9 @@ resource "coder_app" "kasm_vnc" {
   display_name = "kasmVNC"
   url          = "http://localhost:${var.port}"
   icon         = "/icon/kasmvnc.svg"
-  subdomain    = true
+  subdomain    = var.subdomain
   share        = "owner"
+
   healthcheck {
     url       = "http://localhost:${var.port}/app"
     interval  = 5
