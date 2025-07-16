0338bd7821dbf9205521ea44f920db5b149a8ce8
Jash <jashambaliya1@gmail.com>
2025-07-16 14:25:52 +0000

    served over the subdomain by default, and then fallback to localhost

--- registry/AJ0070/modules/pgadmin/main.tf
+++ registry/AJ0070/modules/pgadmin/main.tf
@@ -17,28 +17,31 @@ variable "port" {
   default     = 5050
 }
 
-variable "log_path" {
-  type        = string
-  description = "The path to the log file."
-  default     = "/tmp/pgadmin.log"
+variable "subdomain" {
+  type        = bool
+  description = "If true, the app will be served on a subdomain."
+  default     = true
 }
 
+data "coder_workspace" "me" {}
+
 resource "coder_app" "pgadmin" {
+  count        = data.coder_workspace.me.start_count
   agent_id     = var.agent_id
   display_name = "pgAdmin"
   slug         = "pgadmin"
   icon         = "/icon/postgres.svg"
-  url          = "http://localhost:${var.port}"
+  url          = var.subdomain ? "https://pgadmin-${data.coder_workspace.me.id}.${data.coder_workspace.me.access_url}" : "http://localhost:${var.port}"
   share        = "owner"
 }
 
 resource "coder_script" "pgadmin" {
   agent_id     = var.agent_id
-  display_name = "Install pgAdmin"
+  display_name = "Install and run pgAdmin"
   icon         = "/icon/postgres.svg"
   run_on_start = true
   script = templatefile("${path.module}/run.sh", {
     PORT     = var.port,
-    LOG_PATH = var.log_path,
+    LOG_PATH = "/tmp/pgadmin.log"
   })
 }
