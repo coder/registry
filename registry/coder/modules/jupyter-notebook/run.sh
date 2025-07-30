04735c9c56421c36bcef8a5c4fe101a672af8171
Marcin Tojek <marcin@coder.com>
2025-07-30 10:51:40 +0200

    fix

--- registry/coder/modules/jupyter-notebook/run.sh
+++ registry/coder/modules/jupyter-notebook/run.sh
@@ -24,7 +24,7 @@ fi
 if [ -n "$REQUIREMENTS_PATH" ]; then
   if [ -f "$REQUIREMENTS_PATH" ]; then
     echo "📄 Installing packages from $REQUIREMENTS_PATH..."
-    pip install --no-cache-dir -r "$REQUIREMENTS_PATH"
+    pipx runpip cookiecutter install -r "$REQUIREMENTS_PATH"
     echo "🥳 Packages from $REQUIREMENTS_PATH have been installed\n\n"
   else
     echo "⚠️  REQUIREMENTS_PATH is set to '$REQUIREMENTS_PATH' but the file does not exist!\n\n"
@@ -34,7 +34,7 @@ fi
 # Install packages selected with PIP_INSTALL_EXTRA_PACKAGES
 if [ -n "$PIP_INSTALL_EXTRA_PACKAGES" ]; then
   echo "📦 Installing additional packages: $PIP_INSTALL_EXTRA_PACKAGES"
-  pip install --no-cache-dir $PIP_INSTALL_EXTRA_PACKAGES
+  pipx install $PIP_INSTALL_EXTRA_PACKAGES
   echo "🥳 Additional packages have been installed\n\n"
 fi
 
