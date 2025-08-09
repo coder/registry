dc60cc73e85ff20db6eb0a9915e24b7529ad2422
Harsh panwar <panwarharshwardhan67@gmail.com>
2025-08-09 16:19:27 +0530

    Add task prompt support

--- registry/harsh9485/modules/sourcegraph_amp/scripts/start.sh
+++ registry/harsh9485/modules/sourcegraph_amp/scripts/start.sh
@@ -25,5 +25,14 @@ fi
 cd "$dir"
 echo "Add AMP API key"
 export AMP_API_KEY=$SOURCEGRAPH_AMP_API_KEY
-# Launch AgentAPI server with AMP
-agentapi server --term-width=67 --term-height=1190 -- amp
+
+if [ -n "$SOURCEGRAPH_AMP_TASK_PROMPT" ]; then
+    printf "Running the task prompt: %s\n" "$SOURCEGRAPH_AMP_TASK_PROMPT"
+    PROMPT="Every step of the way, report tasks to Coder with proper descriptions and statuses. Your task at hand: $SOURCEGRAPH_AMP_TASK_PROMPT"
+
+    # Pipe the prompt into amp, which will be run inside agentapi
+    agentapi server --term-width=67 --term-height=1190 -- bash -c "echo \"$PROMPT\" | amp"
+else
+    printf "No task prompt given.\n"
+    agentapi server --term-width=67 --term-height=1190 -- amp
+fi
