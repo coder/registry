1293e1db495efff311cc7baec089a5e673568e00
mavrick-1 <mavrickrishi@gmail.com>
2025-08-13 19:22:08 +0530

    feat: enhance e2e tests with database state verification and test data setup

--- registry/mavrickrishi/modules/aws-ami-snapshot/main.tf
+++ registry/mavrickrishi/modules/aws-ami-snapshot/main.tf
@@ -20,7 +20,7 @@ provider "aws" {
   skip_credentials_validation = true
   skip_requesting_account_id  = true
   skip_region_validation      = true
-  
+
   # Mock credentials for testing
   access_key = "test"
   secret_key = "test"
