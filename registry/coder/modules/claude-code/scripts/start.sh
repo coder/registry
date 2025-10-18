2f0519801eb12b53689faed73ba8e5b3002d9dfa
YEVHENII SHCHERBINA <yevhenii@coder.com>
2025-10-18 01:38:03 +0000

    allowed hosts=*

--- registry/coder/modules/claude-code/scripts/start.sh
+++ registry/coder/modules/claude-code/scripts/start.sh
@@ -111,7 +111,7 @@ function start_agentapi() {
     echo ${BOUNDARY_ARGS[@]} > "/tmp/yevhenii-debug.log"
     echo ${ARGS[@]} >> "/tmp/yevhenii-debug.log"
 
-    agentapi server --type claude --term-width 67 --term-height 1190 -- \
+    agentapi server --allowed-hosts="*" --type claude --term-width 67 --term-height 1190 -- \
       sudo -E env PATH=$PATH setpriv --inh-caps=+net_admin --ambient-caps=+net_admin --bounding-set=+net_admin /home/coder/go/bin/boundary "${BOUNDARY_ARGS[@]}" -- \
       claude
       #"${ARGS[@]}"
