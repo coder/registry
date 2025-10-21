4f35b01cb6b10cc5a91c0460008a8892f93b2d1f
YEVHENII SHCHERBINA <yevhenii@coder.com>
2025-10-21 15:00:08 +0000

    refactor: minor fix

--- registry/coder/modules/claude-code/scripts/start.sh
+++ registry/coder/modules/claude-code/scripts/start.sh
@@ -132,7 +132,7 @@ function start_agentapi() {
     done
 
     agentapi server --allowed-hosts="*" --type claude --term-width 67 --term-height 1190 -- \
-      sudo -E env PATH=$PATH setpriv --inh-caps=+net_admin --ambient-caps=+net_admin --bounding-set=+net_admin /home/coder/go/bin/boundary "${BOUNDARY_ARGS[@]}" -- \
+      sudo -E env PATH=$PATH setpriv --inh-caps=+net_admin --ambient-caps=+net_admin --bounding-set=+net_admin boundary "${BOUNDARY_ARGS[@]}" -- \
       claude "${CLAUDE_ARGS[@]}"
   else
     agentapi server --type claude --term-width 67 --term-height 1190 -- claude "${ARGS[@]}"
