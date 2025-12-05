2ed4be217235403066ad1a83904487bc701d6490
35C4n0r <work.jaykumar@gmail.com>
2025-12-05 18:36:42 +0530

    wip

--- registry/coder/modules/claude-code/main.tf
+++ registry/coder/modules/claude-code/main.tf
@@ -345,7 +345,7 @@ resource "coder_script" "install_agent" {
   agent_id     = var.agent_id
   display_name = "Install agent"
   run_on_start = true
-  log_path = "$HOME/${local.module_dir_name}/install.log"
+  log_path = "~/${local.module_dir_name}/install.log"
   script       = <<EOF
     #!/bin/bash
     set -o errexit
