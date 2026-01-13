483c80413bc8255743cc8a8314434000b53cd7c2
Tao Chen <iamtaochen@outlook.com>
2026-01-13 12:33:01 +0800

    add ssh_key from coder

--- registry/IamTaoChen/templates/ssh-linux/main.tf
+++ registry/IamTaoChen/templates/ssh-linux/main.tf
@@ -55,9 +55,15 @@ data "coder_parameter" "auth_type" {
   }
 
   option {
-    name  = "SSH Key"
+    name  = "SSH Key Manual"
     value = "ssh_key"
   }
+
+  option {
+    name  = "SSH Key from Coder"
+    value = "ssh_key_coder"
+  }
+
 }
 
 data "coder_parameter" "ssh_password" {
@@ -82,6 +88,19 @@ data "coder_parameter" "ssh_key" {
   mutable      = true
   form_type    = "textarea"
   styling = jsonencode({
+    mask_input = true
+  })
+  order = 4
+}
+
+data "coder_parameter" "ssh_key_coder" {
+  count        = data.coder_parameter.auth_type.value == "ssh_key_coder" ? 1 : 0
+  name         = "ssh_key_coder"
+  display_name = "Public Key From Coder"
+  description  = "Add this public key to your remote server's authorized_keys: \n\n${data.coder_workspace_owner.me.ssh_public_key}"
+  default = "********************"
+  styling = jsonencode({
+    disabled = true
     mask_input = true
   })
   order = 4
@@ -125,9 +144,9 @@ locals {
   coder_cache_dir = "${local.home_dir}/.coder/${data.coder_workspace.me.id}"
   agent_id_file   = "${local.coder_cache_dir}/agent.id"
   use_password    = data.coder_parameter.auth_type.value == "password"
-  use_key         = data.coder_parameter.auth_type.value == "ssh_key"
+  use_key         = contains(["ssh_key", "ssh_key_coder"],data.coder_parameter.auth_type.value)
   ssh_password    = local.use_password ? data.coder_parameter.ssh_password[0].value : null
-  ssh_private_key = local.use_key ? data.coder_parameter.ssh_key[0].value : null
+  ssh_private_key = data.coder_parameter.auth_type.value == "ssh_key_coder" ? data.coder_workspace_owner.me.ssh_private_key : (length(data.coder_parameter.ssh_key) > 0 ? data.coder_parameter.ssh_key[0].value : null )
   apps_candidate  = ["VS Code Desktop", "VS Code Web", "Cursor"]
   apps_selected   = (can(data.coder_parameter.apps.value) && data.coder_parameter.apps.value != "") ? jsondecode(data.coder_parameter.apps.value) : []
 }
