498de9ed5c293b59b1c56885318c3c70608dbaed
Harsh panwar <panwarharshwardhan67@gmail.com>
2026-01-21 20:14:48 +0530

    clean up

--- registry/coder/modules/jetbrains/script/install_plugins.sh
+++ registry/coder/modules/jetbrains/script/install_plugins.sh
@@ -109,8 +109,6 @@ log "Waiting for IDE installation. Pending codes: ${pending_codes[*]}"
 MAX_ATTEMPTS=60 # 10 minutes
 attempt=0
 
-log "entering in loop"
-echo "echo :entering in loop"
 # Loop until all plugins installed
 while [ ${#pending_codes[@]} -gt 0 ] && [ $attempt -lt $MAX_ATTEMPTS ]; do
 
@@ -120,8 +118,6 @@ while [ ${#pending_codes[@]} -gt 0 ] && [ $attempt -lt $MAX_ATTEMPTS ]; do
     continue
   fi
 
-  log "Toolbox directory found"
-  echo "echo :Toolbox directory found"
   for product_dir in "$TOOLBOX_BASE"/*; do
     [ -d "$product_dir" ] || continue
 
@@ -157,11 +153,9 @@ while [ ${#pending_codes[@]} -gt 0 ] && [ $attempt -lt $MAX_ATTEMPTS ]; do
     log "Finished $code. Remaining: ${pending_codes[*]:-none}"
 
   done
-  log "one plugin installed"
-  echo "echo :one plugin installed"
   # If still pending, wait and retry
   if [ ${#pending_codes[@]} -gt 0 ]; then
-    sleep 10
+    sleep 60
     ((attempt++))
   fi
 done
